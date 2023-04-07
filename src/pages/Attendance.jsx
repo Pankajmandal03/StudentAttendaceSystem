ff2810ad2bece18475caac882bb13febefef033e
nitish0104 <nitishdalvi1@gmail.com>
2023-04-07 20:25:45 +0530

    working qr code

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -1,36 +1,30 @@
-<<<<<<< HEAD
-import { doc, onSnapshot } from 'firebase/firestore';
-import React, { useEffect } from 'react'
-import { db } from '../Firebase_config';
-import Layout from '../components/Layout';
-import { UserAuth } from '../context/Auth_context';
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { db } from "../Firebase_config";
+import Layout from "../components/Layout";
+import { UserAuth } from "../context/Auth_context";
 
 const Attendance = () => {
-	const {user} =UserAuth()
-	useEffect(() => {
-		const unsub = onSnapshot(doc(db, "AI-1-EC-I", user.uid), (doc) => {
-			console.log("Current data: ", doc.data());
-			unsub()
-		});
-	
-	
-	}, [])
-	
+  const { user } = UserAuth();
+  useEffect(({ qrcodedata }) => {
+    const unsub = onSnapshot(
+      doc(
+        db,
+        `${qrcodedata.department}-${qrcodedata.year}-${qrcodedata.subname}-${qrcodedata.AdminUID}`,
+        user.uid
+      ),
+      (doc) => {
+        console.log("Current data: ", doc.data());
+        unsub();
+      }
+    );
+  }, []);
+
   return (
-	<Layout>
-
-	<div>Attendance</div>
-	</Layout>
-  )
-}
-
-export default Attendance
-=======
-import React from "react";
-
-const Attendance = () => {
-  return <div>your attendance</div>;
+    <Layout>
+      <div>Attendance</div>
+    </Layout>
+  );
 };
 
 export default Attendance;
->>>>>>> 00a8fa524f373e4b3608f8bea57d282e5af41640
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,7 +5,8 @@ import Layout from "../components/Layout";
 import { UserAuth } from "../context/Auth_context";
 import { auth, db } from "../Firebase_config";
 import { Html5Qrcode, Html5QrcodeScanner } from "html5-qrcode";
-import { doc, setDoc } from "@firebase/firestore";
+import { collection, addDoc } from "@firebase/firestore";
+
 import { AiOutlineMenu, AiOutlineScan } from "react-icons/ai";
 import { RxCross1 } from "react-icons/rx";
 
@@ -15,7 +16,7 @@ const Dashboard = () => {
     setexpannd(!expand);
   };
 
-  const { userdata ,user} = UserAuth();
+  const { userdata } = UserAuth();
   const naviGate = useNavigate();
   const logOut = async () => {
     await signOut(auth).then(() => {
@@ -23,75 +24,50 @@ const Dashboard = () => {
     });
   };
 
-  function onScanSuccess(decodedText, decodedResult) {
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-    stopscanning();
-  }
-
-  function onScanFailure(error) {
-    console.warn(`Code scan error = ${error}`);
-    stopscanning();
-  }
-
   const scanbutton = () => {
-  
     const html5QrCode = new Html5Qrcode("reader");
     const qrCodeSuccessCallback = async (decodedText, decodedResult) => {
       /* handle success */
       const qrcodedata = JSON.parse(decodedResult.decodedText);
-      // await setDoc(
-      //   doc(
-      //     db,
-      //     `${qrcodedata.department}-${qrcodedata.year}-${qrcodedata.subname}`,
-      //     "date-starttime-endtime-studentUID"
-      //   ),
-      //   {
-      //     name: "Los Angeles",
-      //     state: "CA",
-      //     country: "USA",
-      //   }
-      // );
-      Html5Qrcode.stop()
-      .then((ignore) => {
-        // QR Code scanning is stopped.
-        console.log(user);
-      })
-      .catch((err) => {
-        alert("stop scanning error");
-      });
-      // stopscanning()
+      html5QrCode
+        .stop()
+        .then(async (ignore) => {
+          await addDoc(
+            collection(
+              db,
+              `${qrcodedata.department}-${qrcodedata.year}-${qrcodedata.subname}`
+            ),
+            {
+              name: `${userdata.name}`,
+              studentId: `${userdata.uid}`,
+              rollno: `${userdata.number}`,
+              teacherid: `${qrcodedata.AdminUID}`,
+            }
+          )
+            .then(() => {
+              alert("data added sucessfully");
+              console.log(qrcodedata);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          alert("stop scanning error");
+        });
     };
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
     html5QrCode.start(
       { facingMode: "environment" },
       config,
-      onScanSuccess,
-      onScanFailure
+      qrCodeSuccessCallback
     );
-  }
-  //   let html5QrcodeScanner = new Html5QrcodeScanner(
-  //     "reader",
-  //     { fps: 10, qrbox: { width: 250, height: 250 } },
-  //     /* verbose= */ false
-  //   );
-  //   html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-  // };
-  const stopscanning = () => {
-    Html5Qrcode.stop()
-      .then((ignore) => {
-        // QR Code scanning is stopped.
-        console.log(user);
-      })
-      .catch((err) => {
-        alert("stop scanning error");
-      });
   };
 
   return (
     <>
-      {/* <Layout className={'flex justify-center items-center'}> */}
       <Layout>
-      <div className="absolute flex-col ">
+        <div className="absolute flex-col ">
           <div className=" pt-4 pl-4  ">
             <button onClick={toggle}>
               <div
@@ -124,20 +100,14 @@ const Dashboard = () => {
                 ) : (
                   <p className="text-2xl hover:font-semibold">Account</p>
                 )}
-                {/* {userdata ? (
-              <h1 className="font-bold text-xl">
-                {userdata.name}
-              </h1>
-            ) : (
-              <h1 className="font-bold text-xl">Account</h1>
-            )} */}
+
                 <li>
                   <Link to="/profile" className="text-2xl hover:font-semibold">
                     Profile
                   </Link>
                 </li>
                 <li>
-                  <Link to="/Attendance" className="text-2xl hover:font-semibold">
+                  <Link to="" className="text-2xl hover:font-semibold">
                     Your Attendance
                   </Link>
                 </li>
@@ -153,8 +123,7 @@ const Dashboard = () => {
         </div>
         <div className="w-full h-full flex justify-center items-center flex-col gap-y-28  ">
           <div className=" h-[50vh] md:h-[55vh] p-2 w-[90vw] md:w-[25vw] border-2 border-dashed  rounded-lg">
-          <div id="reader" width="600px"></div>
-            {/* <p className="flex items-center justify-center w-full h-fit">tap below</p> */}
+            <div id="reader" width="600px"></div>
           </div>
 
           <button
@@ -165,7 +134,6 @@ const Dashboard = () => {
             <AiOutlineScan />
           </button>
         </div>
-    
       </Layout>
     </>
   );
