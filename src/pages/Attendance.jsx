cfdd45cc199c51128526a2dbb54095bc7a36eda6
Prakash8999 <reshavejha@gmail.com>
2023-04-08 08:39:35 +0530

    Did nothing

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -1,24 +1,35 @@
-import { doc, onSnapshot } from "firebase/firestore";
-import React, { useEffect } from "react";
+import { collection, doc, onSnapshot, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { db } from "../Firebase_config";
 import Layout from "../components/Layout";
 import { UserAuth } from "../context/Auth_context";
+import ErrorBoundary from "./ErrorBoundary";
 
 const Attendance = () => {
   const { userdata, user } = UserAuth();
+  const [attendance, setattendance] = useState(null);
 
   useEffect(() => {
+    const temp = [];
     const unsub = onSnapshot(doc(db, "Com-2-EC-I", `${user.uid}`), (doc) => {
+      temp.push(...doc.data() );
       console.log("Current data: ", doc.data());
     });
+    setattendance(temp);
     console.log(user.uid);
     unsub();
   }, []);
 
   return (
-    <Layout>
-      <div>Attendance</div>
-    </Layout>
+
+      <Layout>
+        {attendance && (
+          <div>
+            <p>{attendance.name}</p>
+          </div>
+        )}
+      </Layout>
+  
   );
 };
 
